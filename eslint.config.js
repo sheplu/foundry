51e278fccedac724dd8e016036208bb69c0810b7
Jean Burellier <jean.burellier@gmail.com>
2026-04-26 18:59:08 +0200

    chore: cleanup tokens and themes interfaces

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ export default tseslint.config(
       '**/coverage/**',
       '**/*.tsbuildinfo',
       '**/storybook-static/**',
+      '**/.tsbuild/**',
     ],
   },
   js.configs.recommended,
